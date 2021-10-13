a32ec7f70c740d4b2b504867ef59f4a4fa7c15fe
Karl Michelsen <43517053+Karlo2001@users.noreply.github.com>
2021-10-13 15:13:48 +0200

    Delete commented dead code

--- Assignment4/Program.cs
+++ Assignment4/Program.cs
@@ -15,37 +15,7 @@ namespace Assignment4
     {
         static void Main(string[] args)
         {
-            /*var configuration = LoadConfiguration();
-            var connectionString = configuration.GetConnectionString("ConnectionString");
-
-            var optionsBuilder = new DbContextOptionsBuilder<KanbanContext>().UseSqlServer(connectionString);
-
-            using var context = new KanbanContext(optionsBuilder.Options);*/
-
-            /*var tag = new List<Tag>();
-            var task = new Task { Title = "Cool title", State = State.Active, Tags = tag, Description = "", AssignedTo = null};
-            //Apparently tries to insert into "TaskId" column for some reason. Most likely because Tasks is of type List<Task>
-            var taga = new Tag {
-                Name = "Email@Email.com",
-                Tasks = new List<Task>()
-            };
-            taga.Tasks.Add(task);
-
-            //Console.WriteLine(task);
-            context.Tags.Add(taga);
-            context.SaveChanges();
-            Console.WriteLine(taga);*/
-
-
-            //Only works if tags in Task is of type string else if of type List<Tag> Invalid object name 'TagTask'
-            /*var chars = from c in context.Tasks
-                        where c.Id == 1
-                        select c.Tags;
-
-            foreach (var c in chars)
-            {
-                Console.WriteLine(c);
-            }*/
+            
         }
         static IConfiguration LoadConfiguration()
         {
